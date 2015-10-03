49c56201604ef7f0ef067652f5392910feb1ad4d
guilhermeblf <guilhermeblf@gmail.com>
2015-10-03 20:15:09 -0300

    Revert "Alteração tela login"
    This reverts commit 7b15289f98f66963a05caa60b4f046adeda6f540.

--- HappyHour.Core/Database.cs
+++ HappyHour.Core/Database.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using Cirrious.CrossCore;
+using MvvmCross.Plugins.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Async;
+
+namespace HappyHour.Core
+{
+    public class Database
+    {
+        private SQLiteAsyncConnection _connection;
+
+        public Database()
+        {
+            var connectionFactory = Mvx.Resolve<IMvxSqliteConnectionFactory>();
+            _connection = connectionFactory.GetAsyncConnection("HappyHourDB");
+        }
+
+        internal async void InitDB()
+        {
+            await _connection.CreateTableAsync<Login>();
+        }
+
+        public async void InserirLogin(Login login)
+        {
+            await _connection.InsertAsync(login);
+        }
+
+        public async Task<Login> GetLogin(int id)
+        {
+            return await _connection.Table<Login>().FirstOrDefaultAsync(l => l.Id == id);
+        }
+    }
+}
