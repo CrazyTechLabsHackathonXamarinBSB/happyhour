fb3727b3f04da79053ea3bc683a2273de8ff6c7c
ThaanCosta <thaancosta@gmail.com>
2015-10-03 16:39:34 -0300

    Versão 1

---
+++ HappyHour.Droid/HappyHour/Core/App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Cirrious.CrossCore.Plugins;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace HappyHour.Core
+{
+    internal class App : IMvxApplication
+    {
+        public IMvxViewModelLocator FindViewModelLocator(MvxViewModelRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadPlugins(IMvxPluginManager pluginManager)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
