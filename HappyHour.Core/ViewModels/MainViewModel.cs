b0708e72ffbd543a7a304033cf06e818fa42f019
jose barreto <jose.de.paula@Live.com>
2015-10-04 03:00:34 -0300

    Criação das listas de contatos para windows Universal e Android

--- HappyHour.Core/ViewModels/MainViewModel.cs
+++ HappyHour.Core/ViewModels/MainViewModel.cs
@@ -1,14 +1,21 @@
-﻿using Cirrious.MvvmCross.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Cirrious.CrossCore;
+using Cirrious.MvvmCross.ViewModels;
+using HappyHour.Core.Models;
+using HappyHour.Core.Plugins;
+using System.Collections.ObjectModel;
 
 namespace HappyHour.Core.ViewModels
 {
     public class MainViewModel : MvxViewModel
     {
 
+        public ObservableCollection<GenericContact> Contacts { get; private set; }
+
+        public MainViewModel()
+        {
+            var contacts = Mvx.Resolve<IContacts>();
+            var contactsList = contacts.GetContacts();
+            Contacts = new ObservableCollection<GenericContact>(contactsList);
+        }
     }
 }
---
+++ HappyHour.Droid/Plugins/AndroidContacts.cs
@@ -0,0 +1,49 @@
+using Android.Content;
+using Android.Provider;
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.Droid.Platform;
+using HappyHour.Core.Models;
+using HappyHour.Core.Plugins;
+using System.Collections.Generic;
+
+namespace HappyHour.Droid.Plugins
+{
+    public class AndroidContacts : IContacts
+    {
+        public List<GenericContact> GetContacts()
+        {
+            var genericContacts = new List<GenericContact>();
+
+            var activity = Mvx.Resolve<IMvxAndroidCurrentTopActivity>().Activity;
+            var cr = activity.ContentResolver;
+            var cur = cr.Query(ContactsContract.Contacts.ContentUri, null, null, null, null);
+            if (cur.Count > 0)
+            {
+                while (cur.MoveToNext())
+                {
+                    // read id
+                    var id = cur.GetString(cur.GetColumnIndex(ContactsContract.Contacts.InterfaceConsts.Id));
+                    // read names 
+                    var displayName = cur.GetString(cur.GetColumnIndex(ContactsContract.Contacts.InterfaceConsts.DisplayName));
+                    // Phone Numbers 
+                    var pCur = cr.Query(ContactsContract.CommonDataKinds.Phone.ContentUri, null,
+                            ContactsContract.CommonDataKinds.Phone.InterfaceConsts.ContactId + " = ?", new string[] { id }, null);
+                    while (pCur.MoveToNext())
+                    {
+                        var number = pCur.GetString(pCur.GetColumnIndex(ContactsContract.CommonDataKinds.Phone.Number));
+                        //var typeStr = pCur.GetString(pCur.GetColumnIndex(ContactsContract.CommonDataKinds.Phone.ContentType));
+                        var genericContact = new GenericContact
+                        {
+                            Nome = displayName,
+                            Numero = number
+                        };
+                        genericContacts.Add(genericContact);
+                    }
+                    pCur.Close();
+                }
+            }
+
+            return genericContacts;
+        }
+    }
+}
